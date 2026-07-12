ef83eb0d983e523ca748a63c0453b391e200f8e0
Jay Callicott <jrcallicott@gmail.com>
2026-07-12 14:46:49 -0500

    Initial commit chp12.

---
+++ chp12/mytheme/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './src/js/main.js',
+        styles: './src/css/main.css',
+      },
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
+  },
+});
